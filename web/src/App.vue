c13e88da9a1614ad108a0ec410bd709e7306e3ff
IllTamer <78360471+IllTamer@users.noreply.github.com>
2024-03-21 13:05:42 +0800

    feat: initial web commit

---
+++ web/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+    <lay-switch v-model="active1"></lay-switch>
+ </template>
+
+<script setup lang="ts">
+    import { ref } from 'vue'
+    const active1 = ref(false);
+ </script>
+
+
+
+<style scoped>
+
+</style>
